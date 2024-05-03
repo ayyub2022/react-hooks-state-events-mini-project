f2526eeb663e0b166bfae899076b85a7b70b151e
ayyub2022 <ayyubibrahim22@gmail.com>
2024-05-03 10:26:56 +0300

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,21 @@
-import React from "react";
-import CategoryFilter from "./CategoryFilter";
-import NewTaskForm from "./NewTaskForm";
-import TaskList from "./TaskList";
 
-import { CATEGORIES, TASKS } from "../data";
-console.log("Here's the data you're working with");
-console.log({ CATEGORIES, TASKS });
+import React, { useState, useEffect } from "react";
 
 function App() {
-  return (
-    <div className="App">
-      <h2>My tasks</h2>
-      <CategoryFilter />
-      <NewTaskForm />
-      <TaskList />
-    </div>
-  );
+  const [pic, dog] = useState();
+
+  useEffect(() => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((data) => {
+        dog(data.message);
+      });
+  }, []);
+
+
+  if (!pic) return <p>Loading...</p>;
+
+  return <img src={pic} alt="A Random Dog" />;
 }
 
 export default App;
